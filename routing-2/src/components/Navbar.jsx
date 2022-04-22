e8f96f05258d4a07abc0a1deaba213e7e2f8e651
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-04-22 17:32:49 +0530

    Routing & Navigate

---
+++ routing-2/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+
+
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+
+    const nav = [
+
+        { title: "Home", to: "/" },
+
+        { title: "About", to: "/about" },
+
+        { title: "Product", to: "/product" },
+
+        { title: "Users", to: "/users" },
+         
+        // { title: "Login", to: "/login" },
+
+
+    ];
+
+
+    return (
+
+        <div>
+
+            {
+                nav.map((e, i) => (
+                    <Link key={i} to={e.to} style={{ margin: "10px", color: "tomato" }}  >{e.title}</Link>
+                ))
+            }
+
+        </div>
+
+    );
+
+
+}
