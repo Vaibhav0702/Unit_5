b072c539605d09723ea5dadb17a88e9028848014
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-05-09 11:50:38 +0530

    combine reducer

---
+++ redux-combine_reducer-assignment/src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { Addcounter } from "./Counter.reducer/action";
+
+import { Reducecounter } from "./Counter.reducer/Reducecounter";
+
+// import { store } from "./store";
+
+import { useDispatch, useSelector } from "react-redux";
+
+export const Counter = () => {
+ 
+    const dispatch = useDispatch();
+    
+    const counter = useSelector((store) => store.counter.counter);
+
+    // console.log(state);
+    const handleadd = () => {
+        dispatch(Addcounter(1));
+
+    }
+    const handlereduce = () => {
+        dispatch(Reducecounter(1));
+
+    }
+    return (
+        <>
+            <h1>Counter:{counter}</h1>
+            <button onClick={handleadd}>ADD</button>
+            <button onClick={handlereduce}>reduce</button>
+        </>
+    )
+}
